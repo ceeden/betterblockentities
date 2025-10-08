29a2d2d1950833240b2ac41e6581bf337f5e4d9d
ceeden <emilcedendahl@gmail.com>
2025-10-08 21:25:09 +0200

    Small improvements to RRP system, like caching the pack data

--- src/main/java/betterblockentities/mixin/ResourcePackManagerMixin.java
+++ src/main/java/betterblockentities/mixin/ResourcePackManagerMixin.java
@@ -28,29 +28,9 @@ public class ResourcePackManagerMixin
 {
     @Inject(method = "providePackProfiles", at = @At("RETURN"), cancellable = true)
     private void injectGeneratedPackProfiles(CallbackInfoReturnable<Map<String, ResourcePackProfile>> cir) {
-        ResourceBuilder builder = new ResourceBuilder();
-
-        byte[] packData = builder.buildZip();
-        Pack pack = new Pack("betterblockentities-generated", packData);
-
-        ResourcePackProfile.PackFactory factory = new ResourcePackProfile.PackFactory() {
-            @Override
-            public ResourcePack open(ResourcePackInfo info) {
-                return pack;
-            }
-
-            @Override
-            public ResourcePack openWithOverlays(ResourcePackInfo info, ResourcePackProfile.Metadata metadata) {
-                return pack;
-            }
-        };
-
-        ResourcePackPosition pos = new ResourcePackPosition(true, ResourcePackProfile.InsertionPosition.TOP, true);
-
         Map<String, ResourcePackProfile> map = new TreeMap<>(cir.getReturnValue());
-        ResourcePackProfile generatedProfile = ResourcePackProfile.create(pack.getInfo(), factory, ResourceType.CLIENT_RESOURCES, pos);
-        if (generatedProfile != null) {
-            map.put(generatedProfile.getId(), generatedProfile);
+        if (ResourceBuilder.getPackProfile() != null) {
+            map.put(ResourceBuilder.getPackProfile().getId(), ResourceBuilder.getPackProfile());
         }
         cir.setReturnValue(Map.copyOf(map));
     }
