022ad8bfd527c8db407ab65f1df7d356213cc19e
ceeden <emilcedendahl@gmail.com>
2025-10-08 22:21:57 +0200

    Small optimizations to RRP system, removed setPrettyPrinting from all GSON builders

--- src/main/java/betterblockentities/mixin/ResourcePackManagerMixin.java
+++ src/main/java/betterblockentities/mixin/ResourcePackManagerMixin.java
@@ -8,13 +8,17 @@ import betterblockentities.resource.ResourceBuilder;
 import net.minecraft.resource.*;
 
 /* mixin */
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 /* java/misc */
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 
@@ -26,12 +30,33 @@ import java.util.TreeMap;
 @Mixin(ResourcePackManager.class)
 public class ResourcePackManagerMixin
 {
+    /*
+    @Shadow @Final private Set<ResourcePackProvider> providers;
+
     @Inject(method = "providePackProfiles", at = @At("RETURN"), cancellable = true)
     private void injectGeneratedPackProfiles(CallbackInfoReturnable<Map<String, ResourcePackProfile>> cir) {
         Map<String, ResourcePackProfile> map = new TreeMap<>(cir.getReturnValue());
+
         if (ResourceBuilder.getPackProfile() != null) {
             map.put(ResourceBuilder.getPackProfile().getId(), ResourceBuilder.getPackProfile());
         }
+
         cir.setReturnValue(Map.copyOf(map));
+    }
+     */
+
+    @Inject(
+            method = "providePackProfiles",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/google/common/collect/ImmutableMap;copyOf(Ljava/util/Map;)Lcom/google/common/collect/ImmutableMap;"
+            ),
+            locals = LocalCapture.CAPTURE_FAILSOFT
+    )
+    private void injectGeneratedPackProfiles(CallbackInfoReturnable<Map<String, ResourcePackProfile>> cir, Map<String, ResourcePackProfile> map) {
+        ResourcePackProfile generated = ResourceBuilder.getPackProfile();
+        if (generated != null && !map.containsKey(generated.getId())) {
+            map.put(generated.getId(), generated);
+        }
     }
 }
